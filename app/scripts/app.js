17377eb6912b3d3a55db6153cdfaf08ebbd726ec
Michael Markman <mmarkman@umass.edu>
2015-04-07 16:22:24 -0400

    Headers and basic angular functionality added

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name simpleMaterialDesignPlannerApp
+ * @description
+ * # simpleMaterialDesignPlannerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('simpleMaterialDesignPlannerApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name simpleMaterialDesignPlannerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the simpleMaterialDesignPlannerApp
+ */
+angular.module('simpleMaterialDesignPlannerApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
