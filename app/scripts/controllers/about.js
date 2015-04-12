4ea30b26198bfe9012626bfde05fdd7e727668cd
Michael Markman <mmarkman@umass.edu>
2015-04-12 13:22:28 -0400

    trimming the fat

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -7,11 +7,11 @@
  * # AboutCtrl
  * Controller of the simpleMaterialDesignPlannerApp
  */
-angular.module('simpleMaterialDesignPlannerApp')
-  .controller('AboutCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+//angular.module('simpleMaterialDesignPlannerApp')
+//  .controller('AboutCtrl', function ($scope) {
+//    $scope.awesomeThings = [
+//      'HTML5 Boilerplate',
+//      'AngularJS',
+//      'Karma'
+//    ];
+//  });
