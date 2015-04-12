894529be6347d683d39dfdb275b609fd7542fb19
Michael Markman <mmarkman@umass.edu>
2015-04-12 08:47:01 -0400

    trying to phonegap

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -19,3 +19,18 @@ angular.module('simpleMaterialDesignPlannerApp')
       $scope.todos.splice(index, 1);
     };
   });
+
+var app = {
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, true);
+    },
+
+    onDeviceReady: function() {
+        angular.element(document).ready(function() {
+            angular.bootstrap(document);
+        });
+    },
+};
